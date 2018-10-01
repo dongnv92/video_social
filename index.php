1cbfa7b1a5592596567c585d6ee0acb6b153cdea
Dong Nguyen <nguyenvandong242@gmail.com>
2018-10-01 23:57:15 +0700

    home

--- index.php
+++ index.php
@@ -24,51 +24,13 @@ require_once 'pages/header.php';
             <!-- ... end Main Content -->
             <!-- Left Sidebar -->
             <aside class="col col-xl-3 order-xl-1 col-lg-6 order-lg-2 col-md-6 col-sm-12 col-12">
-
+                <?php echo $funcion->getBlockSideBarIndex();?>
             </aside>
             <!-- ... end Left Sidebar -->
 
             <!-- Right Sidebar -->
             <aside class="col col-xl-3 order-xl-3 col-lg-6 order-lg-3 col-md-6 col-sm-12 col-12">
-                <div class="ui-block">
-                    <div class="ui-block-title">
-                        <h6 class="title">Last Videos</h6>
-                    </div>
-                    <div class="ui-block-content">
-                        <!-- W-Latest-Video -->
-                        <ul class="widget w-last-video">
-                            <li>
-                                <a href="https://vimeo.com/ondemand/viewfromabluemoon4k/147865858" class="play-video play-video--small">
-                                    <svg class="olymp-play-icon">
-                                        <use xlink:href="<?php echo _URL_STYLE;?>/svg-icons/sprites/icons.svg#olymp-play-icon"></use>
-                                    </svg>
-                                </a>
-                                <img src="http://192.168.0.103/dong/video_social/media/images/post/BER8wYaM6U3X.jpg" alt="video">
-                                <div class="video-content">
-                                    <div class="title">System of a Revenge - Hypnotize...</div>
-                                    <time class="published" datetime="2017-03-24T18:18">3:25</time>
-                                </div>
-                                <div class="overlay"></div>
-                            </li>
-                            <li>
-                                <a href="https://youtube.com/watch?v=excVFQ2TWig" class="play-video play-video--small">
-                                    <svg class="olymp-play-icon">
-                                        <use xlink:href="<?php echo _URL_STYLE;?>/svg-icons/sprites/icons.svg#olymp-play-icon"></use>
-                                    </svg>
-                                </a>
-                                <img src="http://192.168.0.103/dong/demo/img/video8.jpg" alt="video">
-                                <div class="video-content">
-                                    <div class="title">Green Goo - Live at Dan’s Arena</div>
-                                    <time class="published" datetime="2017-03-24T18:18">5:48</time>
-                                </div>
-                                <div class="overlay"></div>
-                            </li>
-                        </ul>
-
-                        <!-- .. end W-Latest-Video -->
-                    </div>
-                </div>
-
+                <?php echo $funcion->getBlockSideBarVideo(array('limit' => 5, 'rand' => true));?>
             </aside>
             <!-- ... end Right Sidebar -->
         </div>
