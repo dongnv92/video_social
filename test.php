e13feda6e2d1816b57854d4851f9ffa89e9e84e0
Dong Nguyen <nguyenvandong242@gmail.com>
2018-11-02 17:31:34 +0700

    Company

--- test.php
+++ test.php
@@ -1,5 +1,11 @@
 <?php
+$start_time = microtime(true);
 require_once 'includes/core.php';
-
-$text = $funcion->tiktok_getUrlVideoChina_v2('http://v.douyin.com/R6Cgpu/');
-print_r($text);
+$video  = "291473471675536"; // ID cua VIDEO
+$token  = "";  // access_token cua ban
+$link   = file_get_contents("https://graph.facebook.com/".$video."/?fields=source&access_token=".$token); //lay thong tin graph
+$graph  = json_decode($link, true); // decode json
+echo "<pre>";
+print_r($graph);
+echo "</pre>";
+echo(number_format(microtime(true) - $start_time, 2));
